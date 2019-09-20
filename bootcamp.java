fbaeec622b698d0181febc63e0a5d9c7b885843c
Valentin Garnier <valentingarnier@live.fr>
2019-09-20 11:53:38 +0200

    trying to push

--- bootcamp.java
+++ bootcamp.java
@@ -1,5 +1,11 @@
 public class Bootcamp {
   public static void main(String[] args) {
     System.out.println("Hello, Swd!");
+    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
+
+  }
+
+  public static String computeUltimateQuestion() {
+  	return "still searching...";
   }
 }
