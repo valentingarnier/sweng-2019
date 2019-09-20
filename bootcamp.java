11fcaa8a961369d6dd5be8656d0ddde6e9e4c83f
Valentin Garnier <valentingarnier@live.fr>
2019-09-20 11:50:48 +0200

    universe

--- bootcamp.java
+++ bootcamp.java
@@ -1,5 +1,11 @@
 public class Bootcamp {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello, Wor4ld!");
+    System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
+
   }
+
+  public static int computeUltimateAnswer() {
+	return 6 * 7;
+}
 }
